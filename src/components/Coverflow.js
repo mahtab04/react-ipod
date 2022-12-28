24835cef7206050eb40767d79de26a96ea4a4df2
Mahtab Alam <smmalam9@gmail.com>
2022-12-28 12:08:41 +0000

    project in progress

---
+++ src/components/Coverflow.js
@@ -0,0 +1,16 @@
+
+
+function Coverflow(props) {
+    return (
+      <div id="coverflow">
+          <p>CoverFlow</p>
+      </div>
+    );
+  }
+  
+  export default Coverflow;
+  
+const style = {
+  
+    
+  }
---
+++ src/components/Games.js
@@ -0,0 +1,15 @@
+const Games = (props) => {
+  return (
+    <div id="games">
+      <img style={style} src=
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/3/36/Two_red_dice_01.svg/1024px-Two_red_dice_01.svg.png" alt=""></img>
+    </div>
+  );
+}
+
+export default Games;
+
+const style = {
+  width: 100.5 + '%',
+  height: 184
+}
